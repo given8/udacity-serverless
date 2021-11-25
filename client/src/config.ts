d8e994dbe3fcc2bea0be89746a8c1871e43a69f8
Given <given8@gmail.com>
2021-11-25 14:50:48 +0200

    added request validator and redeployed

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'jotli22eii'
+const apiId = '0rjzgkiwcf'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
